cb3422d876e68a85f179b5a5cfbfb1598b077e48
ahsan722505 <ahsantahir722505@gmail.com>
2022-08-29 23:01:37 +0500

    auth-section

--- routes/auth.js
+++ routes/auth.js
@@ -1,26 +1,39 @@
-const express = require('express');
-const isAuth = require('../middlewares/is-auth');
-const passport=require("passport");
+const express = require("express");
+const isAuth = require("../middlewares/is-auth");
+const passport = require("passport");
 
 const router = express.Router();
-const CLIENT_URL="http://localhost:3000";
+const CLIENT_URL = "http://localhost:3000";
 
-router.get("/getStatus",isAuth,(req,res)=>{
-res.status(200).json({username : req.username})
-})
-
-router.get('/google', passport.authenticate('google', { scope: ['profile'] }));
-
-router.get('/google/callback', 
-passport.authenticate("google", {
-  failureRedirect: CLIENT_URL,
-  session : false
-}),(req,res)=>{
-  const farFuture = new Date(new Date().getTime() + (1000*60*60*24*365*10));
-  res.cookie("token",`${req.user}`,{expires : farFuture});
-  res.redirect(CLIENT_URL);
+router.get("/getStatus", isAuth, (req, res) => {
+  res.status(200).json({ username: req.username });
 });
 
+router.get("/google", (req, res, next) => {
+  const { returnTo } = req.query;
+  const state = Buffer.from(JSON.stringify({ returnTo })).toString("base64");
+  const authenticator = passport.authenticate("google", {
+    scope: ["profile"],
+    state,
+  });
+  authenticator(req, res, next);
+});
 
+router.get(
+  "/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: CLIENT_URL,
+    session: false,
+  }),
+  (req, res) => {
+    const { state } = req.query;
+    const { returnTo } = JSON.parse(Buffer.from(state, "base64").toString());
+    const farFuture = new Date(
+      new Date().getTime() + 1000 * 60 * 60 * 24 * 365 * 10
+    );
+    res.cookie("token", `${req.user}`, { expires: farFuture });
+    res.redirect(returnTo);
+  }
+);
 
 module.exports = router;
